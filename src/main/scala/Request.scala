a1ec85ec85a55e8fc96872a7b5c0209ffc5283a1
yi.han <hanyi8110@gmail.com>
2019-12-25 21:56:43 +0100

    more training

---
+++ src/main/scala/Request.scala
@@ -0,0 +1,23 @@
+package prod
+
+import cats.implicits._
+
+class Cat(val colour: String, val food: String)
+
+
+/*
+Define an object ChipShop with a method willServe. This method should
+accept a Cat and return true if the cat’s favourite food is chips, and false
+otherwise.
+*/ object OperandTester extends App {
+
+    def equalOperator: Boolean = {
+        "" == 123
+    }
+}
+
+object Request
+
+
+
+
